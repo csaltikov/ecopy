2628c8fd4abe385d10056b884c8d78f6e4a1f4f7
csaltikov <saltikov@ucsc.edu>
2025-01-20 20:35:25 -0800

    updated unit tests

--- ecopy/tests/test_ecopy_unittest.py
+++ ecopy/tests/test_ecopy_unittest.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from ecopy import load_data, wt_mean, wt_var, wt_scale, diversity, rarefy
+from ecopy import load_data, wt_mean, wt_var, wt_scale, diversity, rarefy, transform
 
 class TestECOPY(unittest.TestCase):
 
@@ -35,6 +35,22 @@ class TestECOPY(unittest.TestCase):
         BCI = load_data('BCI')
         rareRich = np.round(rarefy(BCI, 'rarefy'))
         self.assertEqual(rareRich[1], 77)
+
+    def test_log_median_ratio(self):
+        counts_arr = np.array(([2, 3, 6],
+                               [10, 11, 24]))
+        # rows are different species, columns are different samples
+        observed_norm = transform(counts_arr, axis=1, method='log_median_ratio')
+        expected_norm = np.array([[4.217, 4.662, 4.273],
+                                  [15.462, 13.986, 15.260]])
+        self.assertTrue(np.array_equal(np.round(expected_norm), np.round(observed_norm)))
+
+        # transposed data the data
+        observed_norm = transform(counts_arr.transpose(), axis=0, method='log_median_ratio')
+        expected_norm = np.array([[4.217, 4.662, 4.273],
+                                  [15.462, 13.986, 15.260]])
+        self.assertTrue(np.array_equal(np.round(expected_norm.transpose()), np.round(observed_norm)))
+
 
 if __name__ == '__main__':
     unittest.main()
